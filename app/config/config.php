bcfff7d5f6247277e94dfbb89f97cdfc6c44dbd0
Phithounsavanh <Phithounsavanh@gmail.com>
2019-04-29 22:37:26 -0400

    Add config/config.php

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+  //App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  //URL Root
+  define('URLROOt', 'http://localhost/lexamvc/');
+  //site name
+  define('Sitename', 'lexamvc');
