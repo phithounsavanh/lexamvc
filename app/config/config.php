81d37695c19b5cd4f1210c538c809041137fba3a
Phithounsavanh <Phithounsavanh@gmail.com>
2019-05-03 11:17:37 -0400

    Create class Database to use PDO to connect to database

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,9 @@
 <?php
+  // DB Params
+  define('DB_HOST','localhost');
+  define('DB_USER','root');
+  define('DB_PASS','123456');
+  define('DB_NAME','lexamvc');
   //App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   //URL Root
